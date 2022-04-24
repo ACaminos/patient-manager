48089e99e3f27da74bdc204da7448a305a4030d1
Ariel Caminos <caminosariel@gmail.com>
2022-04-24 20:52:12 -0300

    install tailwind css and update tailwind.config.js

---
+++ patientManager/tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  purge: ["./index.html","./src/**/*.{jsx}"], //lee el archivo html y va a montar el proyecto de react
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
