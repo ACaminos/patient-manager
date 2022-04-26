cb36023aad65a16fe98c02efe6210e1d3a1743eb
Ariel Caminos <caminosariel@gmail.com>
2022-04-26 15:27:44 -0300

    create component paciente.jsx and downgrade node to version 17

---
+++ patientManager/src/components/Paciente.jsx
@@ -0,0 +1,13 @@
+const Paciente = () => {
+    return(
+        <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+        <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {''} <span className="font-normal normal-case">Hook</span></p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Propietario: {''} <span className="font-normal normal-case">Juan</span></p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Email: {''} <span className="font-normal normal-case">correo@correo.com.ar</span></p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Fecha Alta: {''} <span className="font-normal normal-case">10 Diciembre 2022</span></p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Sintomas: {''} <span className="font-normal normal-case">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit nemo consequatur fugiat consequuntur. Quas sed ratione saepe reiciendis dolor amet sit, ab recusandae quis fuga a, fugit distinctio eum impedit!</span></p>
+    </div>
+    )
+}
+
+export default Paciente
