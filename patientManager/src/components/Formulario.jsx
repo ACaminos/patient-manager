e87b46cf91f4a291c794a9a4e3f90931c9af754a
Ariel Caminos <caminosariel@gmail.com>
2022-04-26 21:25:09 -0300

    useState and onClick

--- patientManager/src/components/Formulario.jsx
+++ patientManager/src/components/Formulario.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 
 function Formulario() {
+  const [nombre, setNombre] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('enviando formulario');
+  }
+
   return (
     <div className='md:w-1/2 lg:w-2/5'>
       <h2 className='font-black text-3xl text-center'>Seguimiento pacientes</h2>
 
       <p className='text-lg mt-5 text-center mb-10'>Añade pacientes y {''} <span className='text-indigo-600 font-bold'>Administralos</span></p>
 
-      <form className='bg-white shadow-md rounded-lg py-10 px-5 mb-10' action="">
+      <form className='bg-white shadow-md rounded-lg py-10 px-5 mb-10' action="" onSubmit={handleSubmit}>
         <div className='mb-5'>
           <label htmlFor='mascota' className='block text-gray-700 uppercase font-bold'>Nombre mascota</label>
-          <input className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md' type="text" name="" id="mascota" placeholder='Nombre de la mascota'  />
+          <input className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md' type="text" name="" id="mascota" value={nombre} onChange={ (e) => setNombre(e.target.value) } placeholder='Nombre de la mascota'  />
         </div>
         <div className='mb-5'>
           <label htmlFor='propietario' className='block text-gray-700 uppercase font-bold'>Nombre propietario</label>
