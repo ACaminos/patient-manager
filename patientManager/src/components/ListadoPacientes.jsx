a92bfa0ec1b80ce571944bd275d56e8217a28423
Ariel Caminos <caminosariel@gmail.com>
2022-05-01 22:36:00 -0300

    add functional buttom Eliminar

--- patientManager/src/components/ListadoPacientes.jsx
+++ patientManager/src/components/ListadoPacientes.jsx
@@ -1,6 +1,6 @@
 import Paciente from "./Paciente";
 
-const ListadoPacientes = ({pacientes, setPaciente}) => {
+const ListadoPacientes = ({pacientes, setPaciente, eliminarPaciente}) => {
 
     return(
         <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
@@ -10,7 +10,14 @@ const ListadoPacientes = ({pacientes, setPaciente}) => {
                     <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
                     <p className="text-xl mt-5 mb-10 text-center">Administra tus {''} <span className="text-indigo-600 font-bold">Pacientes y citas</span></p>
 
-                    {pacientes.map( paciente  => ( <Paciente key={paciente.id} paciente={paciente} setPaciente={setPaciente} /> ))}
+                    {pacientes.map( paciente  => ( 
+                    <Paciente 
+                        key={paciente.id}
+                        paciente={paciente}
+                        setPaciente={setPaciente}
+                        eliminarPaciente={eliminarPaciente} 
+                    /> 
+                    ))}
                 </>
             ) : (
                 <>
